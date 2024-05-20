4c6fc67fea36f35a8a3049e1b6156deb8ab28d7f
Olesia Kapustynska <d.olleska@gmail.com>
2024-05-20 16:41:37 +0300

    Add Angular application

---
+++ src/main/java/com/cfkvyh/CfKvyhApplication.java
@@ -0,0 +1,13 @@
+package com.cfkvyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfKvyhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfKvyhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfkvyh/CfKvyhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfkvyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfKvyhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
