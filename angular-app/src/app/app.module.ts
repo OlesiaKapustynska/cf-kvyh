8e4c83884fd638844f63bbc9ab15a0ac5b1610e9
Olesia Kapustynska <d.olleska@gmail.com>
2024-07-16 20:37:07 +0300

    add router buttons to home page

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import {OurWorkComponent} from "./our-work/our-work.component";
-import {GetInvolvedComponent} from "./get-involved/get-involved.component";
 import {DocumentsComponent} from "./documents/documents.component";
 
 @NgModule({
